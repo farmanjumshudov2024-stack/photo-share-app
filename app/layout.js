c1a9efdc5c788e1699eadce037d1b9e0f935d6d6
farmanjumshudov2024-stack <farmanjumshudov2024@gmail.com>
2026-05-19 06:58:44 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
 
+
+export const metadata = {
+  title: "Photo Share App",
+  description: "Simple photo sharing app"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
